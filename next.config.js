70e36fc053e2b710fe8615d55071bad4de994b3b
unknown <gsmahato>
2023-07-12 14:39:01 +0545

    b

--- next.config.js
+++ next.config.js
@@ -1,13 +1,30 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export",
+//   reactStrictMode: true,
+// };
+
+// module.exports = nextConfig;
+
+// module.exports = {
+//   images: {
+//     domains: ["www.bimaabazar.com"],
+//   },
+// };
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  output: "export",
+  output: 'export',
   reactStrictMode: true,
-};
-
-module.exports = nextConfig;
-
-module.exports = {
   images: {
     domains: ["www.bimaabazar.com"],
   },
-};
+
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
