abea69fbdcff2e1ee79e8afa2c0b87a1fb2840f6
unknown <gsmahato>
2023-07-23 17:39:10 +0545

    mc

--- next.config.js
+++ next.config.js
@@ -1,26 +1,26 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   future: {
-//     webpack5: true,
-//   },
-//   images: {
-//     domains: ["www.bimaabazar.com"],
-//   },
-// };
-
-// module.exports = nextConfig;
-
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
+  future: {
+    webpack5: true,
+  },
   images: {
     domains: ["www.bimaabazar.com"],
   },
 };
 
 module.exports = nextConfig;
+
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
+//   output: 'export',
+//   reactStrictMode: true,
+//   images: {
+//     domains: ["www.bimaabazar.com"],
+//   },
+// };
+
+// module.exports = nextConfig;
 
