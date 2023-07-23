35f3e6aa61d6bf65d3163344ee0ac5883b1a6d4f
unknown <gsmahato>
2023-07-23 08:29:52 +0545

    m

--- next.config.js
+++ next.config.js
@@ -10,15 +10,24 @@
 // };
 
 // module.exports = nextConfig;
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
+//   output: 'export',
+//   reactStrictMode: true,
+//   images: {
+//     domains: ["www.bimaabazar.com"],
+//   },
+// }
+ 
+// module.exports = nextConfig
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
-  images: {
-    domains: ["www.bimaabazar.com"],
-  },
-}
- 
-module.exports = nextConfig
+};
+
+module.exports = nextConfig;
