1d5da5a22a383e23a38649de699be0dc5d5369c7
unknown <gsmahato>
2023-07-27 13:01:40 +0545

    commit

--- src/app/news/getNews.js
+++ src/app/news/getNews.js
@@ -1,5 +1,14 @@
 export async function getNews(id) {
-    const res = await fetch(`https://www.bimaabazar.com/newsportal/news/${id}`);
-    return res.json();
+    try {
+      const res = await fetch(`https://www.bimaabazar.com/newsportal/news/${id}`);
+      if (!res.ok) {
+        // Handle non-200 HTTP status code, if needed
+        throw new Error("Failed to fetch news");
+      }
+      return res.json();
+    } catch (error) {
+      // Handle any other errors that might occur during the fetch
+      console.error("Error fetching news:", error);
+      return null;
+    }
   }
-  
