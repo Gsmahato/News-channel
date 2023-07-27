2bdb09e69e21f6e092ff9480570513e40a7ea045
unknown <gsmahato>
2023-07-27 12:38:30 +0545

    commit

--- src/app/news/[id]/page.js
+++ src/app/news/[id]/page.js
@@ -3,9 +3,7 @@ export default async function Newspage({ params: { id } }){
   const news = await getNews(id)
   console.log(news)
   return(
-    <>
       <div>{news.content}</div>
-    </>
   )
 }
 
